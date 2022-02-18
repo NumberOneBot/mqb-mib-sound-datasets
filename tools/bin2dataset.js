5a6b04c810fa4b8b155b16d4f295e7faf3cc4a7f
Alex Strelets <alex.strelets@gmail.com>
2022-02-18 22:19:08 +0200

    moved XML template file inside the codebase

--- tools/bin2dataset.js
+++ tools/bin2dataset.js
@@ -13,7 +13,6 @@ const {
 	_: files,
 	format = 'bin', // not used
 	zdc = true, // use ZDC prefixes
-	template = 'XMLMSG.xml', // should be in the same folder
 	caret = 16, // add CR every N bytes
 	output = 'DATASET', // default name for output
 	help = false,
@@ -31,51 +30,91 @@ before file extension.
 OPTIONS
   --format=[hex|bin]  format of the input data, default value is 'bin'
   --zdc               use file name prefix as ZDC container name
-  --template=[file]   use this file as template of the dataset, default
-                      is 'XMLMSG.xml'
   --caret=[N]         add CRLF symbol every N bytes, default is 16
   --output=[file]     use it as output dataset file name, default is 'DATASET'
 `);
 	return;
 }
 
+const template = `<?xml version="1.0" encoding="UTF-8"?>
+<MESSAGE DTD="XMLMSG" VERSION="1.1">
+<RESULT>
+<RESPONSE NAME="GetParametrizeData" DTD="RepairHints" VERSION="1.4.7.1" ID="0">
+<DATA>
+<REQUEST_ID>000000000</REQUEST_ID><!--PARAMETERS-->
+<COMPOUNDS>
+<COMPOUND COMPOUND_ID="1">
+<SW_NAME/>
+<SW_VERSION/>
+<SW_PART_NO/>
+</COMPOUND>
+<COMPOUND COMPOUND_ID="2">
+<SW_NAME/>
+<SW_VERSION/>
+<SW_PART_NO/>
+</COMPOUND>
+<COMPOUND COMPOUND_ID="3">
+<SW_NAME/>
+<SW_VERSION/>
+<SW_PART_NO/>
+</COMPOUND>
+<COMPOUND COMPOUND_ID="4">
+<SW_NAME/>
+<SW_VERSION/>
+<SW_PART_NO/>
+</COMPOUND>
+<COMPOUND COMPOUND_ID="5">
+<SW_NAME/>
+<SW_VERSION/>
+<SW_PART_NO/>
+</COMPOUND>
+</COMPOUNDS>
+<INFORMATION>
+<CODE/>
+</INFORMATION>
+<DSD_DATA>
+<COMPRESSED_DATA CONTENT="DSD-Files" CONTENT_TYPE="application/tar" CONTENT_TRANSFER_ENCODING="base64" BYTES_UNCOMPRESSED="0" BYTES_COMPRESSED="0">
+
+</COMPRESSED_DATA>
+</DSD_DATA>
+</DATA>
+</RESPONSE>
+</RESULT>
+</MESSAGE>`;
+
 const prettify = (value, length = 2) => value.toString(16).toUpperCase().padStart(length, '0');
 
-fs.readFile(template, 'utf8', (err, tplData) => {
-	if (err) return console.error(crc.red('\nNo XML template provided'));
-	let parameters = '';
-	let zdcName;
+let parameters = '';
+let zdcName;
 
-	files.map((filename) => {
-		try {
-			zdcName = zdc ? filename.slice(0, filename.indexOf('.')) : '';
-			const binaryData = fs.readFileSync(filename),
-				hexArray = [...binaryData].map((char) => '0x' + prettify(char)),
-				crcData = Uint8Array.from([...binaryData].slice(0, -2)),
-				crc = prettify(crc16Calculator(crcData), 4);
+files.map((filename) => {
+	try {
+		zdcName = zdc ? filename.slice(0, filename.indexOf('.')) : '';
+		const binaryData = fs.readFileSync(filename),
+			hexArray = [...binaryData].map((char) => '0x' + prettify(char)),
+			crcData = Uint8Array.from([...binaryData].slice(0, -2)),
+			crc = prettify(crc16Calculator(crcData), 4);
 
-			hexArray.splice(-2, 2, '0x' + crc.substr(0, 2), '0x' + crc.substr(2, 4));
+		hexArray.splice(-2, 2, '0x' + crc.substr(0, 2), '0x' + crc.substr(2, 4));
 
-			const hexData = hexArray.join(',').replace(new RegExp('(.{' + caret * 5 + '})', 'g'), '$1\n'),
-				address = filename.split('.').slice(-2, -1)[0],
-				parametersName = filename.slice(zdc ? filename.indexOf('.') + 1 : 0, filename.indexOf(address) - 1);
-
+		const hexData = hexArray.join(',').replace(new RegExp('(.{' + caret * 5 + '})', 'g'), '$1\n'),
+			address = filename.split('.').slice(-2, -1)[0],
+			parametersName = filename.slice(zdc ? filename.indexOf('.') + 1 : 0, filename.indexOf(address) - 1),
 			parametersTemplate = `
 <PARAMETER_DATA DIAGNOSTIC_ADDRESS="0x5F" START_ADDRESS="${address}" PR_IDX="" ZDC_NAME="${zdcName}" ZDC_VERSION="0001" LOGIN="20103" LOGIN_IND="" DSD_TYPE="1" SESSIONNAME="" FILENAME="${parametersName}">
 ${hexData}
 </PARAMETER_DATA>`;
-			parameters += parametersTemplate;
-		} catch (err) {
-			console.log('Error reading binary files', err);
-			parameters = false;
-		}
-	});
-	if (parameters) {
-		const data = tplData.replace('<!--PARAMETERS-->', parameters);
-		const outName = zdcName || output;
-		fs.writeFile(`${outName}.xml`, data, (err) => {
-			if (err) return console.log(err);
-			console.log(clc.cyan(`${outName}.xml`) + ' created');
-		});
+		parameters += parametersTemplate;
+	} catch (err) {
+		console.log('Error reading binary files', err);
+		parameters = false;
 	}
 });
+if (parameters) {
+	const data = template.replace('<!--PARAMETERS-->', parameters),
+		outName = zdcName || output;
+	fs.writeFile(`${outName}.xml`, data, (err) => {
+		if (err) return console.log(err);
+		console.log(clc.cyan(`${outName}.xml`) + ' created');
+	});
+}
