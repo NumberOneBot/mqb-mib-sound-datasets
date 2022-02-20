620367ca420d38bfebc4fe0bbe4ef1037d2bf7b6
Alex Strelets <alex.strelets@gmail.com>
2022-02-20 19:26:27 +0200

    initial VCP containers support for bin2dataset

--- tools/bin2dataset.js
+++ tools/bin2dataset.js
@@ -9,12 +9,14 @@ if (!argv._.length) {
 	console.log(clc.yellow('\nNo file name(s) provided'));
 	argv.help = true;
 }
+
 const {
 	_: files,
 	format = 'bin', // not used
 	zdc = true, // use ZDC prefixes
 	caret = 16, // add CR every N bytes
 	output = 'DATASET', // default name for output
+	container = 'odis',
 	help = false,
 } = argv;
 
@@ -27,15 +29,16 @@ Packs one or several data blocks into dataset. Names of the files should contain
 address of the block as last suffix before file extension.
 
 OPTIONS
-  --format=[hex|bin]  format of the input data, default value is 'bin'
-  --zdc               use file name prefix as ZDC container name
-  --caret=[N]         add CRLF symbol every N bytes, default is 16
-  --output=[file]     use it as output dataset file name, default is 'DATASET'
+  --format=[hex|bin]     format of the input data, default is 'bin'
+  --container=[odis|vcp] format of the resulting container, default is 'odis'
+  --zdc                  use file name prefix as ZDC container name
+  --caret=[N]            add CRLF symbol every N bytes, default is 16
+  --output=[file]        use it as output dataset file name, default is 'DATASET'
 `);
 	return;
 }
 
-const template = `<?xml version="1.0" encoding="UTF-8"?>
+const odisTemplate = `<?xml version="1.0" encoding="UTF-8"?>
 <MESSAGE DTD="XMLMSG" VERSION="1.1">
 <RESULT>
 <RESPONSE NAME="GetParametrizeData" DTD="RepairHints" VERSION="1.4.7.1" ID="0">
@@ -81,6 +84,23 @@ const template = `<?xml version="1.0" encoding="UTF-8"?>
 </RESULT>
 </MESSAGE>`;
 
+const vcpTemplate = `<ZDC>
+<IDENT>
+<DATEINAME>ASAM_ODX</DATEINAME>
+<DATEIID>V09600047P6</DATEIID>
+<VERSION-TYP></VERSION-TYP>
+<VERSION-INHALT>0204</VERSION-INHALT>
+<LOGIN>20103</LOGIN>
+<ALFID>24</ALFID>
+<PRNRREF></PRNRREF>
+<DATUM></DATUM>
+<BESCHREIBUNG></BESCHREIBUNG>
+</IDENT>
+<DATENBEREICHE>
+<!--PARAMETERS-->
+</DATENBEREICHE>
+</ZDC>`;
+
 const prettify = (value, length = 2) => value.toString(16).toUpperCase().padStart(length, '0');
 
 let parameters = '';
@@ -98,20 +118,34 @@ files.map((filename) => {
 
 		const hexData = hexArray.join(',').replace(new RegExp('(.{' + caret * 5 + '})', 'g'), '$1\n'),
 			address = filename.split('.').slice(-2, -1)[0],
+			dataSize = '0x' + binaryData.length.toString(16),
 			parametersName = filename.slice(zdc ? filename.indexOf('.') + 1 : 0, filename.indexOf(address) - 1),
-			parametersTemplate = `
+			parametersVcpTemplate = `<DATENBEREICH>
+<DATEN-NAME>${zdcName}</DATEN-NAME>
+<DATEN-FORMAT-NAME>DFN_HEX</DATEN-FORMAT-NAME>
+<START-ADR>${address}</START-ADR>
+<GROESSE-DEKOMPRIMIERT>${dataSize}</GROESSE-DEKOMPRIMIERT>
+<DATEN>
+${hexData}
+</DATEN>
+</DATENBEREICH>`,
+			parametersOdisTemplate = `
 <PARAMETER_DATA DIAGNOSTIC_ADDRESS="0x5F" START_ADDRESS="${address}" PR_IDX="" ZDC_NAME="${zdcName}" ZDC_VERSION="0001" LOGIN="20103" LOGIN_IND="" DSD_TYPE="1" SESSIONNAME="" FILENAME="${parametersName}">
 ${hexData}
 </PARAMETER_DATA>`;
-		parameters += parametersTemplate;
+		if (container === 'odis') {
+			parameters += parametersOdisTemplate;
+		} else {
+			parameters += parametersVcpTemplate;
+		}
 	} catch (err) {
 		console.log('Error reading binary files', err);
 		parameters = false;
 	}
 });
 if (parameters) {
-	const data = template.replace('<!--PARAMETERS-->', parameters),
-		outName = zdcName || output;
+	const data = (container === 'odis' ? odisTemplate : vcpTemplate).replace('<!--PARAMETERS-->', parameters),
+		outName = (zdcName ? zdcName + '.' : '') + output + '.' + container.toUpperCase();
 	fs.writeFile(`${outName}.xml`, data, (err) => {
 		if (err) return console.log(err);
 		console.log(clc.cyan(`${outName}.xml`) + ' created');
